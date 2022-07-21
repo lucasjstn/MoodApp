2d4951eb745fd73b925fc9187c53eda2c5ad3368
lucasjstn <lucas7justino@gmail.com>
2022-07-21 07:13:19 -0300

    login page retornando acesstoken e httpcode

--- src/telas/Login.js
+++ src/telas/Login.js
@@ -1,27 +1,90 @@
-import React from 'react';
-import { Image, KeyboardAvoidingView, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { TextInput, Image, KeyboardAvoidingView, StatusBar, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Entrada from '../componentes/Entrada'
 
-
 const Login = ({navigation}) => {
+
+    const [params, setParams] = useState('');
+    const [status, setStatus] = useState(0);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("lucas7@email.com");
+    const [password, setPassword] = useState("senhasenha");
+
+
+    const FazerLogin = () => {
+        
+
+        axios
+            .post("https://shrouded-shelf-01513.herokuapp.com/oauth/token", 
+                {
+                    "grant_type": "password",
+                    "email": email,
+                    "password": password,
+                    "client_id": "3mGWGtxIEKyhq_HGG4cq6hsTOd_zn1SuTD3_cafjUPc",
+                    "client_secret": "389JLi1Nd6DQ_soCI85C57ueTlMZ_JR7pRq6SJ0GaB0",
+                }
+            )
+            .then(response => {
+                // params = response?.data.access_token;
+                // console.log(response?.data.access_token, "\n",response?.data.refresh_token)
+                setStatus(response.status);
+                setParams (response?.data.access_token);
+            })
+            .catch(error => { setStatus(error.response.status)})
+    }
+
     return (
         <KeyboardAvoidingView style={styles.container}>
             <StatusBar barStyle={'dark-content'} backgroundColor={'white'}/>
+           <TextInput 
+                style={{
+                    width: '80%',
+                    backgroundColor: 'white',
+                    color: 'black',
+                    margin: 10,
+                }}
 
-            <Image 
-                
+                defaultValue={name}
+                onChangeText={text => setName(text)}
+           />
+
+            <TextInput 
+                style={{
+                    width: '80%',
+                    backgroundColor: 'white',
+                    color: 'black',
+                    margin: 10,
+                }}
+
+                defaultValue={email}
+                onChangeText={text => setEmail(text)}
             />
 
-            <Entrada placeholder='email' keyboardType='email-address'/> 
-            <Entrada Senha placeholder='senha'/>
-
-            <TouchableOpacity activeOpacity={0.8} style={styles.botao_entrar} onPress={() => navigation.navigate('BottomTab')}>
-                <Text>Entrar</Text>
-            </TouchableOpacity>
+            <TextInput 
+                style={{
+                    width: '80%',
+                    backgroundColor: 'white',
+                    color: 'black',
+                    margin: 10,
+                }}
+                
+                defaultValue={password}
+                onChangeText={text => setPassword(text)}
+            />
+           
+           <Button title='console' onPress={() => {console.log(name)}}/>
+           <Button title='login' onPress={FazerLogin}/>
+           <Text>{email}</Text>
+           <Text>{password}</Text>
+           <Text>{params}</Text>
+           <Text>HTTP: {status}</Text>
 
         </KeyboardAvoidingView>
     )
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
