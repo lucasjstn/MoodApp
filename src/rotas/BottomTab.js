c17da47869b133b82cb7187556918c5678396d3d
lucasjstn <lucas7justino@gmail.com>
2022-07-25 17:49:12 -0300

    componente provisorio bbottom tab corrigido

--- src/rotas/BottomTab.js
+++ src/rotas/BottomTab.js
@@ -1,33 +1,93 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './Home';
 import Perfil from '../telas/Perfil'
-import { Modal, View } from 'react-native';
+import { Modal, Text, View, TextInput, TouchableOpacity, Pressable} from 'react-native';
+import { darkBlue, lightBlue, lightColor } from '../constantes';
 
-const BottomTab = () => {
+const BottomTab = ({navigation}) => {
     const Tab = createBottomTabNavigator();
 
     return (
       	<Tab.Navigator initialRouteName='Home' backBehavior='firstRoute'
-			 screenOptions={{ headerShown: false, tabBarShowLabel: false, }} defaultScreenOptions={{ tabBarStyle: {} }}>
+			 screenOptions={{ headerShown: false, tabBarShowLabel: false, }} defaultScreenOptions={{}}>
 
-			 <Tab.Screen name='Home' component={Home} options={{ tabBarIconStyle: { left: -40 } }} />
-			 <Tab.Screen name='Add' component={Provisorio}/> 
-			 <Tab.Screen name='Perfil' component={Perfil} />
+			 <Tab.Screen name='Home' component={Home} options={{ unmountOnBlur: true, }} />
+			 <Tab.Screen name='Add' component={Provisorio} options={{unmountOnBlur: true,}} /> 
+			 <Tab.Screen name='Perfil' component={Perfil}/>
 
 		 </Tab.Navigator>
 		 	
     )
 }
 
-function Provisorio(){
-	return (
-		<Modal style={{justifyContent: 'center', alignItems: 'center'}}> 
-			<View style={{width: 100, height: 100, backgroundColor: 'black'}}>
+function Provisorio({navigation}){
 
+	const [show, setShow] = useState(false);
+	const [selected, setSelected] = useState(false);
+
+	const toHome = () => {
+		navigation.navigate('Home');
+	}
+
+	useEffect(() => {
+		setShow(true);
+	}, [])
+
+    return (
+
+        <Modal 
+		hardwareAccelerated
+            visible={show}
+			
+        > 
+            <View style={{flex: 1,height: '100%',backgroundColor: lightColor}}>
+
+				<TouchableOpacity style={{borderRadius: 10,justifyContent: 'center',width: 40, height: 40, backgroundColor: lightBlue, alignItems: 'center'}} onPress={toHome}>
+					<Text style={{fontSize: 25, color: darkBlue}}>X</Text>
+				</TouchableOpacity>
+				<Text>Como voce está?</Text>
+				<Text>hoje, 23 de janeiro</Text>
+				<Text>8:35</Text>
+
+				<View style={{flexDirection: 'row', justifyContent: 'center'}}>
+					<Pressable onFocus={null} onPress={()=>{setSelected(true)}} style={selected ? {width: 50, height: 50, backgroundColor: 'blue', margin: 10}: {width: 50, height: 50, backgroundColor: 'red', margin: 10}}>
+
+					</Pressable>
+					<TouchableOpacity style={{width: 50, height: 50, backgroundColor: 'blue', margin: 10}}>
+
+					</TouchableOpacity>
+					<TouchableOpacity style={{width: 50, height: 50, backgroundColor: 'blue', margin: 10}}>
+
+					</TouchableOpacity>
+					<TouchableOpacity style={{width: 50, height: 50, backgroundColor: 'blue', margin: 10}}>
+
+					</TouchableOpacity>
+					<TouchableOpacity style={{width: 50, height: 50, backgroundColor: 'blue', margin: 10}}>
+
+					</TouchableOpacity>
+
+					
+				</View>
+				<View style={{borderRadius: 10, backgroundColor: 'white', borderColor: 'black', borderWidth: 1, height: '50%', width: '90%', alignSelf: 'center'}}>
+
+				</View>
+
+				
+
+				<View style={{top: 20,borderRadius: 10, backgroundColor: 'white', borderColor: 'black', borderWidth: 1, height: '10%', width: '90%', alignSelf: 'center'}}>
+					<TextInput placeholder='escreva aqui o que aconteceu'/>
+				</View>
+				
+				<View style={{top: 50}}>
+				<TouchableOpacity style={{position: 'relative',backgroundColor: darkBlue, width: '90%', alignSelf: 'center', alignItems: 'center', top: '100%'}}>
+					<Text>Salvar</Text>
+				</TouchableOpacity>
+				</View>
 			</View>
-		</Modal>
-	)
+        </Modal>
+        
+    )
 }
 
 export default BottomTab;
--- src/telas/Perfil.js
+++ src/telas/Perfil.js
@@ -1,12 +1,123 @@
 import React from "react";
-import { View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { darkBlue, fontePadrao, lightColor } from "../constantes";
 
 const Perfil = () => {
     return(
-        <View style={{backgroundColor: 'black', color: 'white', height: '100%', alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{color: 'white'}}>Perfiasdasdasdl</Text>
+        <View style={styles.container}>
+
+            <View style={styles.image}>
+                
+            </View>
+
+            <View style={{backgroundColor: 'pink', maxWidth: '80%', height: 100, width: '80%', alignSelf: 'center', height: 50, position: 'absolute', top: 350}}>
+
+                <View style={styles.info}>
+                    <Text style={styles.info.titulo}>E-mail:</Text>
+                    <Text style={styles.info.content} numberOfLines={1}>Emailmsm@email.commmmmmmmmmm</Text>
+                </View>
+
+                <View style={styles.info}>
+                    <Text style={styles.info.titulo}>gênero:</Text>
+                    <Text style={styles.info.content} numberOfLines={1}>feminino</Text>
+                </View>
+
+                <View style={styles.info}>
+                    <Text style={styles.info.titulo}>data de nascimento:</Text>
+                    <Text style={styles.info.content} numberOfLines={1}>15/15/2015</Text>
+                </View>
+
+                    <TouchableOpacity style={styles.botaoSalvar}>
+                        <Text style={styles.botaoSalvar.texto}>Editar perfil</Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity style={styles.botaoSair}>
+                        <Text style={styles.botaoSair.texto}>sair</Text>
+                    </TouchableOpacity>
+            
+            </View>
+            <Text style={styles.ola}>olá, fulana</Text>
+
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    botaoSair: {
+        alignSelf: 'center',
+        width: '133%',
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: 'black',
+        texto: {
+            color: 'black',
+            fontWeight: 'bold',
+            fontFamily: fontePadrao,
+            textTransform: 'uppercase',  
+        },
+    },
+    botaoSalvar: {
+        alignSelf: 'center',
+        width: '133%',
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        backgroundColor: '#304FFE',  
+            texto: {
+                color: lightColor,
+                fontWeight: 'bold',
+                fontFamily: fontePadrao,
+                textTransform: 'uppercase',  
+            },
+    },
+    info: {
+        // maxWidth: 200,
+        width: 300,        // position: 'absolute',
+        flexDirection: 'row',
+        left: -30,
+        // top: 350,
+        // left: 42,
+        titulo: {
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            margin: 2,
+
+        },
+        content:{
+            fontWeight: '400',
+            textTransform: 'uppercase',
+            margin: 2,
+        },
+    },
+    ola: {
+        position: 'absolute',
+        color: 'black',
+        fontWeight: '400',
+        top: 300,
+        fontSize: 30,
+        fontFamily: 'Source Sans Pro',
+        textTransform: 'capitalize'
+    },
+    image:{
+        backgroundColor: 'pink',
+        width: 128,
+        height: 128,
+        borderRadius: 128/2,
+        top: 128, 
+    },
+    container: {
+        height: '100%',
+        backgroundColor: lightColor,
+        alignItems: 'center',
+    }
+})
 
 export default Perfil;
